72cb03d4f19e4a963cbdefef72a178999f1261a6
Jonathon <Jonathon.esbri@gmail.com>
2022-04-13 13:14:55 -0400

    ADDED SOURCE CODE

--- spm/settings.py
+++ spm/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'spm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -126,6 +126,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
